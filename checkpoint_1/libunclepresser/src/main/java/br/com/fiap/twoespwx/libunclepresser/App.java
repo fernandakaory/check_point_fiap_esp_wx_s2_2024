2187a13946f1dd3e6e05af729e645b69ab939b59
fernandakaory <fernandakaory2@gmai.com>
2024-10-04 21:02:49 -0300

    adicionando código teste para escrita do output

--- checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
+++ checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
@@ -3,21 +3,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
- * Grupo: Batata Doce
+ * Grupo: ABOBRINHA
  * Autores: 
- *      - João da Silva     -   jaodasilva@example.com
- *      - Jose da Silva     -   zedasilva@example.com
- *      - Maria da Silva    -   marydasilva@example.com
- *      - Monica da Silva   -   nicadasilva@example.com
+ *      - David Denunci  (RM98603)   -  rm98603@fiap.com.br
+ *      - Fernanda Saito (RM551105)   -   rm551104@fiap.com.br
+ *      - Lucas de Toledo  (RM97913)-   rm97913@fiap.com.br
+ *      - Pedro Gava (RM551043) -   rm551043@fiap.com.br
  */
 
 package br.com.fiap.twoespwx.libunclepresser;
 
-
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println("LIB UNCLE PRESSER - GRUPO BATATA-DOCE");
+public class App {
+    public static void main(String[] args) {
+        System.out.println("LIB UNCLE PRESSER - GRUPO ABOBRINHA");
     }
 }
