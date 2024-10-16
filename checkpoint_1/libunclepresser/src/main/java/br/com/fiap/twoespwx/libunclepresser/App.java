00331e95cd88fe3b6a1d8f294f8436524df2bc12
fernandakaory <fernandakaory2@gmail.com>
2024-10-16 18:56:36 -0300

    arrumando comentários

--- checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
+++ checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/App.java
@@ -1,3 +1,18 @@
+/* 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Grupo: Abobrinha
+ * Autores: 
+ *      - David Denunci (RM98603)     -   rm98603@fiap.com.br
+ *      - Fernanda Saito (RM551105)     -   rm551104@fiap.com.br
+ *      - Lucas de Toledo (RM97913)   -   rm97913@fiap.com.br
+ *      - Pedro Gava (RM551043)   -   rm551043@fiap.com.br
+ * 
+ * 
+ */
+
 package br.com.fiap.twoespwx.libunclepresser;
 
 import java.io.*;
@@ -6,25 +21,27 @@ public class App {
     public static void main(String[] args) {
         System.out.println("LIB UNCLE PRESSER - GRUPO ABOBRINHA");
 
-        // Gerar e salvar a sequência de nucleotídeos
+        // gera e salva a sequencia de nucleotideos
+        // definição dos caminhos dos txts de entrada e saída
         String nucleotides = Compressor.generateSequence();
         String inputFilePath = "checkpoint_1\\libunclepresser\\inputs\\input1.txt";
         String outputFilePath = "checkpoint_1\\libunclepresser\\outputs\\OUTPUT1.TXT";
 
+        // tratamento de erros obrigatório para casos de erro nos arquivos de entrada ou saída
         try {
-            // Escrever a sequência de nucleotídeos no arquivo de entrada
+            // chama o método de escrita da saída para inserir a sequencia gerada no arquivo de input
             Compressor.outputWriter(inputFilePath, nucleotides);
 
-            // Ler o arquivo de entrada usando BufferedReader
+            // nucleotideo agora recebe a sequencia de entrada 
             nucleotides = Compressor.inputReader(inputFilePath);
 
-            // Compressão usando o algoritmo RLE
+            // atribuição dos dados compressados a uma variável
             String compressedData = Compressor.compressRLE(nucleotides);
 
-            // Escrever a saída no arquivo de saída
+            // insere a saída no arquivo
             Compressor.outputWriter(outputFilePath, compressedData);
-
-            // Exibir informações sobre o processo
+            
+            // exibe o resumo
             Compressor.displaySummary(inputFilePath, outputFilePath, nucleotides, compressedData);
 
         } catch (IOException e) {
--- checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/Compressor.java
+++ checkpoint_1/libunclepresser/src/main/java/br/com/fiap/twoespwx/libunclepresser/Compressor.java
@@ -6,8 +6,13 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Compressor {
+    // declaração de variáveis globais para calculo de frequencia
+    public static int freqA;
+    public static int freqC;
+    public static int freqG;
+    public static int freqT;
 
-    // Método para gerar uma sequência aleatória de nucleotídeos
+    // gera a sequeência de nucleotídeo de entrada
     public static String generateSequence() {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
@@ -18,34 +23,31 @@ public class Compressor {
         char[] sequence = new char[quantity];
         char[] nucleotides = {'A', 'C', 'G', 'T'};
         
-        // Primeiro nucleotídeo é escolhido aleatoriamente
         sequence[0] = nucleotides[random.nextInt(nucleotides.length)];
         
-        // Definindo uma probabilidade de repetir o nucleotídeo anterior
-        double repeatProbability = 0.7;  // 70% de chance de repetir o nucleotídeo anterior
+        //definindo uma probabilidade de repetir o nucleotídeo anterior, para tornar mais parecido com as sequências reais e as usadas de exemplo
+        double repeatProbability = 0.7;  
 
         for (int i = 1; i < quantity; i++) {
             if (random.nextDouble() < repeatProbability) {
-                // Repetir o nucleotídeo anterior
                 sequence[i] = sequence[i - 1];
             } else {
-                // Escolher um novo nucleotídeo aleatório
                 sequence[i] = nucleotides[random.nextInt(nucleotides.length)];
             }
         }
 
-        // Exibir a sequência gerada (apenas para debug)
         System.out.print("Sequência de nucleotídeos gerada: ");
         for (char nucleo : sequence) {
             System.out.print(nucleo);
         }
-        System.out.println(); // Nova linha após a sequência gerada
+        System.out.println();
 
         scanner.close();
         return new String(sequence);
     }
 
-    // Método que realiza a compressão usando Run-Length Encoding (RLE)
+    // método que faz a compressão rle
+    // referencia https://www.dca.fee.unicamp.br/~martino/disciplinas/ea978/na4.pdf
     public static String compressRLE(String data) {
         StringBuilder compressed = new StringBuilder();
 
@@ -57,7 +59,6 @@ public class Compressor {
                 i++;
             }
             compressed.append(data.charAt(i));
-            // Adiciona o número de repetições apenas se for maior que 1
             if (count > 1) {
                 compressed.append(count);
             }
@@ -66,7 +67,7 @@ public class Compressor {
         return compressed.toString();
     }
 
-    // Método para contar a frequência de nucleotídeos
+    // método para contar a frequencia dos caracteres da sequencia
     public static void countFrequency(String input) {
         freqA = 0;
         freqC = 0;
@@ -96,7 +97,8 @@ public class Compressor {
         }
     }
 
-    // Método para leitura usando InputStreamReader e BufferedReader
+    //método de para ler uma entrada usando buffread
+    // referencia: https://www.devmedia.com.br/amp/leitura-e-escrita-de-arquivos-de-texto-em-java/25529
     public static String inputReader(String path) throws IOException {
         BufferedReader buffRead = new BufferedReader(new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
         StringBuilder sb = new StringBuilder();
@@ -108,7 +110,7 @@ public class Compressor {
         return sb.toString();
     }
 
-    // Método para escrita usando OutputStreamWriter e BufferedWriter
+    // método de escrita de saída usando buffwrite
     public static void outputWriter(String path, String content) throws IOException {
         BufferedWriter buffWrite = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(path), StandardCharsets.UTF_8));
         buffWrite.write(content);
@@ -130,10 +132,8 @@ public class Compressor {
         System.out.printf("| TOTAL INPUT CHARACTERS: %d                                 |\n", originalData.length());
         System.out.println("|                                                           |");
 
-        // Chamada ao método de contagem de frequências
         countFrequency(originalData);
 
-        // Exibindo as frequências calculadas
         System.out.printf("| A: %d (%.2f%%)                                              |\n", freqA, (freqA * 100.0 / originalData.length()));
         System.out.printf("| C: %d (%.2f%%)                                              |\n", freqC, (freqC * 100.0 / originalData.length()));
         System.out.printf("| T: %d (%.2f%%)                                              |\n", freqT, (freqT * 100.0 / originalData.length()));
@@ -149,9 +149,5 @@ public class Compressor {
         System.out.println(" -----------------------------------------------------------");
     }
 
-    // Atributos para contagem de nucleotídeos
-    public static int freqA;
-    public static int freqC;
-    public static int freqG;
-    public static int freqT;
+
 }
